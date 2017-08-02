feb83999ba1379af17328704624b685ba0df784a
gilbert.wang <gilbert.wang@vipshop.com>
2017-08-02 12:10:07 +0800

    init version

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+﻿#ifndef __BINARY_LOG_AGENT_H__
+#define __BINARY_LOG_AGENT_H__
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc , char ** argv) {
+	printf("%s\n" , "hello world");
+
+	return 0;
+}
+
+#endif
+
+
+
