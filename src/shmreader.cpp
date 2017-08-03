1bf5236056485daf4f22ccd7fb84a8fe932d3f61
gilbert.wang <gilbert.wang@vipshop.com>
2017-08-03 21:54:59 +0800

    1

---
+++ src/shmreader.cpp
@@ -0,0 +1,40 @@
+﻿#include "shmreader.h"
+
+#include <stdlib.h>
+
+using namespace binlog;
+using namespace com::vip::local::cache::proto;
+
+const static int DEFAULT_PATH_LENGTH = 256;
+
+ShmReader::ShmReader():m_indexlock(0){
+}
+
+ShmReader::~ShmReader(){
+	if (m_indexlock != 0) {
+		delete m_indexlock;
+	}
+}
+
+bool ShmReader::initialize() {
+	char lockPath[DEFAULT_PATH_LENGTH] = {0};
+
+	char * path = getenv("DISTRIBUTED_STRING_LOCAL_CACHE_INDEX_PATH");
+	if (path == 0) {
+		(void)sprintf(lockPath , "%s" , "index.lock");
+	} else {
+		(void)sprintf(lockPath , "%s/%s" , path , "index.lock");
+	}
+
+	m_indexlock = new FileLock(lockPath);
+
+	return true;
+}
+
+bool ShmReader::read(SharedMemoryObject & object) {
+	return true;
+}
+
+
+
+
---
+++ src/shmreader.h
@@ -0,0 +1,22 @@
+﻿#ifndef __SHM_READER_H__
+#define __SHM_READER_H__
+
+#include "shared_memory_struct.pb.h"
+#include "filelock.h"
+
+namespace binlog {
+	class ShmReader {
+		public:
+			ShmReader();
+			~ShmReader();
+
+			bool initialize();
+
+			bool read(com::vip::local::cache::proto::SharedMemoryObject & object); 
+		private:
+			FileLock * m_indexlock;
+	};
+}
+
+#endif
+
