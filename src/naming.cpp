11b207cb0dcade13bf97749af675ba493e7a467d
gilbert.wang <gilbert.wang@vipshop.com>
2017-08-09 18:46:12 +0800

    1

--- src/naming.cpp
+++ src/naming.cpp
@@ -3,14 +3,54 @@
 
 using namespace binlog;
 using namespace common;
+using namespace std;
 
 NamingService * NamingService::m_instance = 0;
 
-NamingService::NamingService(){
+static const int DEFAULT_NAMING_SDK_PORT = 10012;
+
+int naming_callback(int fd , char * buffer) {
+	return 0;
+}
+
+NamingService::NamingService() {
 }
 
 
 NamingService::~NamingService() {
+}
+
+bool NamingService::add(string host) {
+	map<string , Connector *>::iterator it = m_peers.find(host);
+	if (it != m_peers.end()) {
+		return false;		
+	}
+
+	Connector * connector = new Connector();
+
+	connector->initialize(it->first , DEFAULT_NAMING_SDK_PORT , naming_callback);
+
+	(void)m_peers.insert(std::make_pair<std::string , Connector *>(host , connector));
+
+	return true;
+}
+
+bool NamingService::remove(string host) {
+	map<string , Connector *>::iterator it = m_peers.find(host);
+	if (it == m_peers.end()) {
+		return false;		
+	}
+
+	Connector * connector = it->second;
+
+	connector->closeConnector();
+
+	delete connector;
+
+	(void)m_peers.erase(it);
+
+	return true;
+
 }
 
 NamingService * NamingService::instance() {
--- src/naming.h
+++ src/naming.h
@@ -1,11 +1,14 @@
 ﻿#ifndef __NAMING_H__
 #define __NAMING_H__
 
+#include <map>
+#include "connector.h"
+
 namespace binlog {
 	class NamingService {
 		public:
 			NamingService();
-			~NamingService();
+			virtual ~NamingService();
 
 			static NamingService * instance();
 
@@ -13,8 +16,13 @@ namespace binlog {
 
 			static bool callback(char * buffer , int & length);
 
+			bool remove(std::string host);
+			bool add(std::string host);
+
 		private:
 			static NamingService * m_instance;
+
+			std::map<std::string , Connector *> m_peers;
 	};
 }
 
